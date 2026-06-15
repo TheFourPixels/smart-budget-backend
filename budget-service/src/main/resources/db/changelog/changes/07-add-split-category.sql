33bda3542428219b134866c40958879bdc87940a
Valerii_Butko <lb2005lba@gmail.com>
2026-06-15 11:15:06 +0500

    add(changes): добалено изменение в changelog

--- budget-service/src/main/resources/db/changelog/changes/07-add-split-category.sql
+++ budget-service/src/main/resources/db/changelog/changes/07-add-split-category.sql
@@ -1,3 +1,1 @@
-INSERT INTO categories (id, user_id, name, is_system)
-VALUES (-1, 0, 'Разделенная транзакция', TRUE)
-ON CONFLICT (id) DO NOTHING;
+﻿INSERT INTO categories (id, user_id, name, is_system) VALUES (-1, 0, 'Разделенная транзакция', TRUE) ON CONFLICT (id) DO NOTHING;
